c9413d1af8541b0107b9ac651f3de5263ff72170
Tracy Kubert <tracykubert@tracys-mbp.lan>
2017-07-07 09:00:17 -0400

    Adding in JawsDB

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,8 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// Picked up from 22-MVC class activity in handlebars section
+
 // Helper function for SQL syntax.
 function printQuestionMarks(num) {
   var arr = [];
